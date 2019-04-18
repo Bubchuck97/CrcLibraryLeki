c5ba7d35c05413c4493bf032705db2cdb6678b2c
mateusz <mateuszleki.7@gmail.com>
2019-04-18 16:02:35 +0200

    added book model

---
+++ src/LibraryLekiWebApi/Models/BookMeasurement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryLekiWebApi.Models
+{
+    public class BookMeasurement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(13)")]
+        public string Isbn { get; set; }
+
+        [Required]
+        public DateTime Published { get; set; }
+
+    }
+}
