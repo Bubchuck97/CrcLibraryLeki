e513d13672925a85c486e1bb39353bbe0f1ad506
mateusz <mateuszleki.7@gmail.com>
2019-04-18 20:44:31 +0200

    added book repository

---
+++ src/LibraryLekiWebApi/Repository/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using LibraryLekiWebApi.DbContexts;
+using LibraryLekiWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LibraryLekiWebApi.Repository
+{
+    public class BookRepository : IBookRepository<Book>
+    {
+        private readonly BookContext _dbContext;
+
+        public BookRepository(BookContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Book entity)
+        {
+            await _dbContext.Books.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(Book book)
+        {
+            _dbContext.Books.Remove(book);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Book> Get(uint id)
+        {
+            return await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            return await _dbContext.Books.ToListAsync();
+        }
+
+        public async Task Update(Book book, Book entity)
+        {
+            book.Isbn = entity.Isbn;
+            book.Published = entity.Published;
+            book.Title = entity.Title;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/LibraryLekiWebApi/Repository/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryLekiWebApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LibraryLekiWebApi.Repository
+{
+    public interface IBookRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> Get(uint id);
+        Task Add(TEntity entity);
+        Task Update(Book book, TEntity entity);
+        Task Delete(Book book);
+    }
+}
